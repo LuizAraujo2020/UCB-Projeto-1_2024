c99ab65927d78d2ebaa8e679ae30cb07ff5b40a6
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2024-05-04 22:03:16 -0300

    ensure LoginRouter returns 401 when invalid credentials is provided

--- profiler-site/src/presentation/routers/login-router.js
+++ profiler-site/src/presentation/routers/login-router.js
@@ -21,5 +21,10 @@ module.exports = class LoginRouter {
         }
 
         this.authUseCase.auth(email, password)
+
+        return {
+            statusCode: 401,
+            body: 'Login realizado com sucesso'
+        }
     }
 }
