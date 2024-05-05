298e18f1c9af87751a66b56fc1531331f6123315
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2024-05-05 01:23:25 -0300

    ensure LoginRouter returns 500 if EmailVlaidor throw

--- profiler-site/src/presentation/routers/login-router.spec.js
+++ profiler-site/src/presentation/routers/login-router.spec.js
@@ -29,6 +29,15 @@ const makeEmailValidator = () => {
     return emailValidatorSpy
 }
 
+const makeThrowingEmailValidator = () => {
+    class EmailValidatorSpy {
+        isValid(email) {
+            throw new Error()
+        }
+    }
+    return new EmailValidatorSpy()
+}
+
 const makeAuthUseCase = () => {
     class AuthUseCaseSpy {
         async auth(email, password) {
@@ -201,6 +210,20 @@ describe('Login Router', () => {
         expect(httpResponse.statusCode).toBe(500)
         expect(httpResponse.body).toEqual(new ServerError())
     }) 
+    
+    test('Should return 500 if EmailValidator throws', async () => {
+        const authUseCaseSpy = makeAuthUseCase()
+        const emailValidatorSpy = makeThrowingEmailValidator()
+        const sut = new LoginRouter(authUseCaseSpy, emailValidatorSpy)
+        const httpRequest = {
+            body: {
+                email: 'any_email@email.com',
+                password: 'any_password'
+            }
+        }
+        const httpResponse = await sut.route(httpRequest)
+        expect(httpResponse.statusCode).toBe(500)
+    })
     
     test('Should call AuthUseCase with correct params', async () => {
         const { sut, authUseCaseSpy } = makeSUT()
