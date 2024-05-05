f840f172602d893dfc30a4bb2b6b749139cde093
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2024-05-04 21:59:14 -0300

    ensure LoginRouter calls AuthUseCase with correct params

--- profiler-site/src/presentation/routers/login-router.js
+++ profiler-site/src/presentation/routers/login-router.js
@@ -1,6 +1,10 @@
 const HTTPStatusCode = require('../helpers/http-status-code')
 
 module.exports = class LoginRouter {
+    constructor(authUseCase) {
+        this.authUseCase = authUseCase
+    }
+    
     route(httpRequest) {
         if(!httpRequest || !httpRequest.body) {
             return HTTPStatusCode.internalServer()
@@ -15,5 +19,7 @@ module.exports = class LoginRouter {
         if(!password) {
             return HTTPStatusCode.badRequest('password')
         }
+
+        this.authUseCase.auth(email, password)
     }
 }
