1c67de9fcf3bcf81b75a6e9eb9e3f774e5685a43
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2024-05-15 23:22:07 -0300

    movegeneric errors to util layer

--- profiler-site/src/presentation/routers/login-router.js
+++ profiler-site/src/presentation/routers/login-router.js
@@ -1,5 +1,5 @@
 const HTTPStatusCode = require('../helpers/http-status-code')
-const { MissingParamError, InvalidParamError } = require('../errors')
+const { MissingParamError, InvalidParamError } = require('../../utils/errors')
 
 module.exports = class LoginRouter {
     constructor(authUseCase, emailValidator) {
