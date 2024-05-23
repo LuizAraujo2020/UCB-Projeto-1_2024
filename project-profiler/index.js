479ef17b3459cf44ab1a02aa7873b34c4f0ae15c
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2024-05-23 12:07:46 -0300

    rename main folder

---
+++ project-profiler/index.js
@@ -0,0 +1,58 @@
+//= ===== IMPORTAÇÃO DE MÓDULOS / DEPENDÊNCIAS
+const express = require('express')
+const mustacheExpress = require('mustache-express')
+
+const userController = require('./src/controllers/userController')
+
+const Errors = require('./src/models/error')
+
+//= ===== CONFIGURAÇÃO INICIAL
+const app = express()
+
+app.engine('html', mustacheExpress())
+app.set('view engine', 'html')
+app.set('views', __dirname + '/src/views')
+
+// Pasta estática para expor arquivos ao html: css, imagens e etc.
+app.use(express.static('static'))
+
+//= ===== ROTAS
+app.get('/', function (request, response) {
+  response.render('search')
+})
+
+app.get('/signup', function (request, response) {
+  response.render('signup')
+})
+
+app.get('/login', function (request, response) {
+  response.render('login')
+})
+
+app.post('/settings', function (request, response) {
+  const user = request.query.user
+
+  response.render('settings', { user })
+})
+
+app.get('/:username', function (request, response) {
+  const username = request.params.username
+
+  const user = userController.findUserByUsername(username)
+
+  // Se não for encontrado um Usuário com o `username` passado pela URL, vai para tela de Busca de Usuário.
+  if (!user) {
+    const error = Errors.Users.userNotFound
+    response.render('search', { error })
+    return
+  }
+
+  response.render('index', { user })
+})
+
+//= ===== EXECUTAR O SERVIDOR
+const PORT = 8080
+
+app.listen(PORT, function () {
+  console.log('Servidor rodando na porta: ' + PORT)
+})
---
+++ project-profiler/src/models/user.js
@@ -0,0 +1,24 @@
+const mockUsers = [
+  {
+    username: 'fulanadetal',
+    name: 'Fulana',
+    surname: 'de Tal',
+    email: 'fulana_tal@email.com'
+  },
+  {
+    username: 'fulanodetal',
+    name: 'Fulano',
+    surname: 'de Tal',
+    email: 'fulano_tal2@email.com'
+  },
+  {
+    username: 'abrilinasantos',
+    name: 'Abrilina',
+    surname: 'Santos',
+    email: 'abrilina@email.com'
+  }
+]
+
+module.exports = {
+  mockUsers
+}
