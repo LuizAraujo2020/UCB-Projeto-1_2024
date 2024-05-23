f7eceb53b4d314124bc12f5c63d9cdbe92121479
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2024-05-23 11:56:18 -0300

    clean the project

--- profiler-site/src/presentation/errors/server-error.js
+++
@@ -1,6 +0,0 @@
-module.exports = class ServerError extends Error {
-    constructor() {
-        super(`An Internal error occurred, please try again later.`)
-        this.name = 'ServerError'
-    }
-}
