f4d299ce316b96e96c84869b74ab3b922724f49d
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2024-05-23 12:42:14 -0300

    add express, mustache, and basic structure

--- project-profiler/index.js
+++ project-profiler/index.js
@@ -1,56 +1,30 @@
-//= ===== IMPORTAÇÃO DE MÓDULOS / DEPENDÊNCIAS
+//==== IMPORTAÇÃO DE MÓDULOS / DEPENDÊNCIAS
 const express = require('express')
 const mustacheExpress = require('mustache-express')
 
 const userController = require('./src/Controllers/userController')
-
 const Errors = require('./src/Models/error')
 
-//= ===== CONFIGURAÇÃO INICIAL
+
+//==== CONFIGURAÇÃO INICIAL
 const app = express()
 
 app.engine('html', mustacheExpress())
 app.set('view engine', 'html')
-app.set('views', __dirname + '/src/views')
+app.set('views', __dirname + '/src/Views')
 
+// Decode dos dados vindos do POST
+app.use(express.urlencoded({ extended: true }))
 // Pasta estática para expor arquivos ao html: css, imagens e etc.
-app.use(express.static('static'))
+app.use(express.static('Static'))
 
-//= ===== ROTAS
-app.get('/', function (request, response) {
-  response.render('search')
-})
 
-app.get('/signup', function (request, response) {
-  response.render('signup')
-})
+//==== ROTAS
+const router = require('./src/Utils/Routes/mainRoutes')
+app.use('/', router)
 
-app.get('/login', function (request, response) {
-  response.render('login')
-})
 
-app.post('/settings', function (request, response) {
-  const user = request.query.user
-
-  response.render('settings', { user })
-})
-
-app.get('/:username', function (request, response) {
-  const username = request.params.username
-
-  const user = userController.findUserByUsername(username)
-
-  // Se não for encontrado um Usuário com o `username` passado pela URL, vai para tela de Busca de Usuário.
-  if (!user) {
-    const error = Errors.Users.userNotFound
-    response.render('search', { error })
-    return
-  }
-
-  response.render('index', { user })
-})
-
-//= ===== EXECUTAR O SERVIDOR
+//==== EXECUTAR O SERVIDOR
 const PORT = 8080
 
 app.listen(PORT, function () {
---
+++ project-profiler/src/controllers/searchController.js
@@ -0,0 +1,9 @@
+function search(req, res) {
+    //TODO: Fazer toda a lógica
+
+    res.render('search')
+}
+
+module.exports = {
+    search
+}
